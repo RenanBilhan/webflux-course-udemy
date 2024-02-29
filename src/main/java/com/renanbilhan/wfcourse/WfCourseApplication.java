d257e4faaecd94c24a115ecde3f8dd9438d96123
RenanBilhan <renan.bilhan@hotmail.com>
2024-02-29 10:55:37 -0300

    Initial commit

---
+++ src/main/java/com/renanbilhan/wfcourse/WfCourseApplication.java
@@ -0,0 +1,13 @@
+package com.renanbilhan.wfcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WfCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WfCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renanbilhan/wfcourse/WfCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renanbilhan.wfcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WfCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
